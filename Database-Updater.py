33bb140313139947d43146771fac3c1876ab86a2
Vlad Feldfix <62237119+VladFeldfix@users.noreply.github.com>
2024-11-24 08:23:56 +0200

    Add files via upload

--- Database-Updater.py
+++ Database-Updater.py
@@ -6,7 +6,7 @@ class main:
     # constructor
     def __init__(self):
         # load smart console
-        self.sc = SmartConsole("Database Updater", "2.0")
+        self.sc = SmartConsole("Database Updater", "2.1")
 
         # set-up main memu
         self.sc.add_main_menu_item("RUN", self.run)
@@ -497,7 +497,7 @@ class main:
         workbook.close()
         self.sc.print("")
         self.sc.good("Done!")
-        self.sc.exit()
+        
         # restart
         self.sc.restart()
 
