93e82126d1455e7de0db9e00bca00b1123a480c8
Vlad Feldfix <62237119+VladFeldfix@users.noreply.github.com>
2025-03-12 11:49:02 +0200

    Add files via upload

--- Database-Updater.py
+++ Database-Updater.py
@@ -6,12 +6,14 @@ from functions.Jigs import Jigs
 from functions.Products import Products
 from functions.Test_Logs import Test_Logs
 from functions.Generate_Excel import Generate_Excel
+import datetime
+import time
 
 class main:
     # constructor
     def __init__(self):
         # load smart console
-        self.sc = SmartConsole("Database Updater", "3.0")
+        self.sc = SmartConsole("Database Updater", "3.1")
 
         # set-up main memu
         self.sc.add_main_menu_item("RUN", self.run)
@@ -32,40 +34,54 @@ class main:
         # display main menu
         self.sc.start()
     
+    def gettime(self):
+        ts = time.time()
+        return datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+
     def run(self):
-        self.sc.print("Processing...")
+        ts = self.gettime()
+        self.sc.print(ts+" Processing...")
         self.sc.print("\nGATHERING DATA:")
         
-        self.sc.print("Gathering data on programs...")
+        ts = self.gettime()
+        self.sc.print(ts+" Gathering data on programs...")
         self.data_programs = Programs.get_data(self.path_programs, self.path_test_results)
         
-        self.sc.print("Gathering data on test cables...")
+        ts = self.gettime()
+        self.sc.print(ts+" Gathering data on test cables...")
         self.data_testingcables = TestingCables.get_data(self.path_test_cables)
         
-        self.sc.print("Gathering data on jigs...")
+        ts = self.gettime()
+        self.sc.print(ts+" Gathering data on jigs...")
         self.data_jigs = Jigs.get_data(self.path_test_cables+"/JIGS.csv")
         
-        self.sc.print("Gathering data on products...")
+        ts = self.gettime()
+        self.sc.print(ts+" Gathering data on products...")
         self.data_products = Products.get_data(self.path_programs)
         
-        self.sc.print("Gathering data on test logs...")
+        ts = self.gettime()
+        self.sc.print(ts+" Gathering data on test logs...")
         self.data_testlogs = Test_Logs.get_data(self.path_test_results)
-
-        self.sc.print("Generating Lua files...")
-        TestingCables.makelua(self.path_test_cables)
         
-        self.generate_excel_table()
+        self.generate_files()
     
-    def generate_excel_table(self):
+    def generate_files(self):
         self.sc.print("\nGENERATING DATA:")
-        self.sc.print("Generating Excel table...")
+
+        ts = self.gettime()
+        self.sc.print(ts+" Generating Lua files...")
+        TestingCables.makelua(self.path_test_cables)
+
+        ts = self.gettime()
+        self.sc.print(ts+" Generating Excel table...")
         xl = Generate_Excel()
         xl.setup_sheets(self.path_database)
         xl.write_data(self.data_programs, self.data_testingcables, self.data_jigs, self.data_products, self.data_testlogs)
         
         # finish
+        ts = self.gettime()
         self.sc.print("")
-        self.sc.good("Done!")
+        self.sc.good(ts+" Done!")
         
         # restart
         self.sc.restart()
